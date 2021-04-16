783488cf83eaa90fc0cb6e9396ee56a95beff939
paunem <paulius.nemeiksis@gmail.com>
2021-04-16 14:58:09 +0300

    added user controller

---
+++ backend/JiperBackend/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json.Linq;
+using JiperBackend.DAL;
+using JiperBackend.Models;
+
+namespace JiperBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ReactPolicy")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataLoader dataLoader;
+        public UserController(DataLoader dataLoader)
+        {
+            this.dataLoader = dataLoader;
+        }
+
+        [HttpPost("register")]
+        public void Add([FromBody] JObject data)
+        {
+            int id = data["id"].ToObject<int>();
+            string email = data["email"].ToObject<string>();
+            string password = data["password"].ToObject<string>();
+            string firstName = data["firstName"].ToObject<string>();
+            string lastName = data["lastName"].ToObject<string>();
+            string phoneNumber = data["phoneNumber"].ToObject<string>();
+            string city = data["city"].ToObject<string>();
+            string streetAddress = data["streetAddress"].ToObject<string>();
+            string zipCode = data["zipCode"].ToObject<string>();
+            dataLoader.AddUser(new User(id, email, password, firstName, lastName, phoneNumber, city, streetAddress, zipCode));
+        }
+    }
+}
--- backend/JiperBackend/Models/User.cs
+++ backend/JiperBackend/Models/User.cs
@@ -13,5 +13,18 @@ namespace JiperBackend.Models
         public string City { get; set; }
         public string StreetAddress { get; set; }
         public string ZipCode { get; set; }
+
+        public User(int id, string email, string password, string firstName, string lastName, string phoneNumber, string city, string streetAddress, string zipCode)
+        {
+            Id = id;
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            City = city;
+            StreetAddress = streetAddress;
+            ZipCode = zipCode;
+        }
     }
 }
