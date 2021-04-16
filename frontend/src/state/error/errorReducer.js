9e814a54309d58c803782224115dfca1e0ba45d9
Ignas-S <samoilovas.ignas@gmail.com>
2021-04-16 19:53:41 +0300

    spread in reducer

--- frontend/src/state/error/errorReducer.js
+++ frontend/src/state/error/errorReducer.js
@@ -9,8 +9,8 @@ export default function authReducer(state = initialState, action) {
   switch(action.type) {
   case SET_ERROR:
     return {
-      error: action.payload.error,
-      message: action.payload.message || '',
+      ...state,
+      ...action.payload
     }
   default:
     return state
