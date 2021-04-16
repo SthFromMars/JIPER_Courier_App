8ea882f2be0a48b8651a836efc9eacffa1804013
paunem <paulius.nemeiksis@gmail.com>
2021-04-16 18:34:07 +0300

    Address entity, user Id db generated

---
+++ backend/JiperBackend/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JiperBackend.Models
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNr { get; set; }
+        public string ZipCode { get; set; }
+
+        public Address(string city, string street, string houseNr, string zipCode)
+        {
+            City = city;
+            Street = street;
+            HouseNr = houseNr;
+            ZipCode = zipCode;
+        }
+    }
+}
--- backend/JiperBackend/Models/User.cs
+++ backend/JiperBackend/Models/User.cs
@@ -1,30 +1,26 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JiperBackend.Models
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string City { get; set; }
-        public string StreetAddress { get; set; }
-        public string ZipCode { get; set; }
+        public Address Address { get; set; }
 
-        public User(int id, string email, string password, string firstName, string lastName, string phoneNumber, string city, string streetAddress, string zipCode)
+        public User(string email, string password, string firstName, string lastName, string phoneNumber, Address address)
         {
-            Id = id;
             Email = email;
             Password = password;
             FirstName = firstName;
             LastName = lastName;
             PhoneNumber = phoneNumber;
-            City = city;
-            StreetAddress = streetAddress;
-            ZipCode = zipCode;
+            Address = address;
         }
     }
 }
