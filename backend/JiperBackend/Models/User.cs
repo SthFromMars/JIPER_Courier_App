936c845002f6e77623bd2f1405a273b4898cd6fe
paunem <paulius.nemeiksis@gmail.com>
2021-04-16 14:20:05 +0300

    User entity created

---
+++ backend/JiperBackend/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JiperBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string StreetAddress { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
