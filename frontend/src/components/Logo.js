138749d779b5a12a3b17a81dcacda6c62d7868a1
Jokubas Ruskys <jokubas.ruskys@gmail.com>
2021-04-20 14:15:11 +0300

    fix typo

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -6,7 +6,7 @@ export default function Logo() {
   return (
     <Row className="py-5 justify-content-center">
       {/* TODO: Replace this garbage with an actual logo if we get around to making one */}
-      <h1 className="display-4 logo-text " >Courrier</h1>
+      <h1 className="display-4 logo-text " >Courier</h1>
       <h1 className="display-4 logo-text bg-primary text-white">App</h1>
     </Row>
   );
